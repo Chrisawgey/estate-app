d0d0f1c6ead7ff5a807159f7a412465c7ba6d3b1
Chrisawgey <chrisvpopoca@gmail.com>
2024-01-25 22:18:40 -0500

    final rough draft

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -80,7 +80,7 @@ const Contact = () => {
         {/* right side */}
         <div className="flexEnd c-right">
           <div className="image-container">
-            <img src="./contact.jpg" alt="" />
+            <img src="./r2.png" alt="" />
           </div>
         </div>
       </div>
